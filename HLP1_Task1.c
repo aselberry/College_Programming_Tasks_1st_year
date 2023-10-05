0e953ea46b8872ca6b43ac7d2a86ee18859050a8
Asel Temiralieva <125737339+aselberry@users.noreply.github.com>
2023-10-05 15:19:50 +0200

    Update HLP1_Task1.c

--- HLP1_Task1.c
+++ HLP1_Task1.c
@@ -22,24 +22,3 @@ int main() {
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-int delete_digits(char *a){
-    int len = strlen(a);
-    for(int i=0; i<len; i++){
-        if(isdigit(a[i])){
-            a[i] = a[i+1];
-        }
-        else
-            printf("%c", a[i]);
-    }
-}
-
-int main() {
-    char str[] = "This is a string with spaces";
-    printf("The return value: %c ", delete_digits(str));
-    return 0;
-}
